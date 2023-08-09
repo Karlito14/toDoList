d70765abaf9b071dd498a94937fc515724928036
Karlito14 <leiroz26@hotmail.com>
2023-08-09 16:13:25 +0200

    réalisation du layout

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { style } from "./App.style";
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={style.app}>
+            <View style={style.header}>
+              <Text>Header</Text>
+            </View>
+            <View style={style.body}>
+              <Text>Body</Text>
+            </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={style.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
+    
+  )
+}
+
+
---
+++ App.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    app : {
+        backgroundColor : '#f9f9f9',
+        flex : 1
+    },
+    header : {
+        flex : 1
+    },
+    body : {
+        flex : 5
+    },
+    footer : {
+        height : 70
+    }
+});
